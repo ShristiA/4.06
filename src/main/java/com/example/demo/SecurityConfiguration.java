81bd9e1bbfa293f2df7a82322bf671235c2dd282
ShristiA <shristiamatya3@gmail.com>
2018-11-04 23:01:20 -0500

    its 403 lesson

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -8,31 +8,45 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
+    public static BCryptPasswordEncoder passEncoder(){
+        return new BCryptPasswordEncoder();
+
+    }
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/")
+                .access("hasAnyAuthority(('USER','ADMIN')")
+                .antMatchers("/admin").access("hasAuthority('ADMIN')")
+                .anyRequest().authenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                 .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").permitAll();
+    }
 
         @Override
-        protected void configure(AuthenticationManagerBuilder auth)throws Exception{
-            auth.inMemoryAuthentication().withUser("user").password(
-                    encoder.encode("password")).authorities("USER");
+        protected void configure(AuthenticationManagerBuilder auth)throws Exception {
+            auth.inMemoryAuthentication()
+                    .withUser("dave").password(encoder().encode("password")).authorities("ADMIN")
+                    .and()
+                    .withUser("user").password(encoder().encode("password")).authorities("USER").and().passwordEncoder(encoder());
 
 
         }
-@Override
-    protected void configure(HttpSecurity http) throws Exception {
-    http
-            .authorizeRequests()
-            .anyRequest().authenticated()
-            .and()
-            .formLogin();
-}
+
 
     }
 
