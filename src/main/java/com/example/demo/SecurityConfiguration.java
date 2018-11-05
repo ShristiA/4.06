aaf270dfc0e3614ab3e00fa59970ed12b1c8a99f
GBTC406002UR <GBTC406002UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-11-05 17:17:50 -0500

    chapter 4

--- src/main/java/com/example/demo/SecurityConfiguration.java
+++ src/main/java/com/example/demo/SecurityConfiguration.java
@@ -1,11 +1,13 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -15,34 +17,42 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Bean
-    public static BCryptPasswordEncoder passEncoder(){
+    public PasswordEncoder encoder(){
         return new BCryptPasswordEncoder();
+    }
+    @Autowired
+    private SSUserDetailsService userDetailService;
 
-    }
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    @Autowired
+    private  UserRepository userRepository;
+
     @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+            return new SSUserDetailsService(userRepository);
+    }
+     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/")
-                .access("hasAnyAuthority(('USER','ADMIN')")
-                .antMatchers("/admin").access("hasAuthority('ADMIN')")
+                .antMatchers("/","/h2-console/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin().loginPage("/login").permitAll()
                  .and()
                 .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").permitAll();
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login").permitAll().permitAll()
+                .and()
+                .httpBasic();
+
+           http
+              .csrf().disable();
+        http
+                .headers().frameOptions().disable();
     }
 
         @Override
         protected void configure(AuthenticationManagerBuilder auth)throws Exception {
-            auth.inMemoryAuthentication()
-                    .withUser("dave").password(encoder().encode("password")).authorities("ADMIN")
-                    .and()
-                    .withUser("user").password(encoder().encode("password")).authorities("USER").and().passwordEncoder(encoder());
+                    auth.userDetailsService(userDetailsServiceBean()).passwordEncoder(encoder());
 
 
         }
---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,113 @@
+package com.example.demo;
+
+
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.*;
+import java.util.Collection;
+
+@Entity
+@Table(name="User_data")
+public class User {
+    @Id@GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(name="email", nullable = false)
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name = "enabled")
+    private boolean enabled;
+
+    @Column(name = "username")
+    private String userName;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(joinColumns = @JoinColumn(name = "user_id"),inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Collection<Role> roles;
+
+    public User(){
+    }
+     public User(String email, String password, String firstName, String lastName, boolean enabled, String username){
+        this.setEmail(email);
+        this.setPassword(password);
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setEnabled(enabled);
+        this.setUserName(username);
+     }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        BCryptPasswordEncoder passEncoder = new BCryptPasswordEncoder();
+        this.password = passEncoder.encode(password);
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Collection<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Collection<Role> roles) {
+        this.roles = roles;
+    }
+}
